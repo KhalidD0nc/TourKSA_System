a4effbd54223875cf3e0d128ce924c5905990adf
Khalid <khalidald4z@gmail.com>
2024-09-08 11:55:11 +0300

    Initial Commit

---
+++ SWE_Project/SWE_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWE_ProjectApp.swift
+//  SWE_Project
+//
+//  Created by Khalid R on 05/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct SWE_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
